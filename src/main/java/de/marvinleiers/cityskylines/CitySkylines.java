77def5d367f93c344d3b1f8b09e17aabf425b221
ML <marvinleiers@gmail.com>
2020-06-17 00:18:01 +0200

    Improved tab-completion

--- src/main/java/de/marvinleiers/cityskylines/CitySkylines.java
+++ src/main/java/de/marvinleiers/cityskylines/CitySkylines.java
@@ -69,6 +69,9 @@ public final class CitySkylines extends JavaPlugin implements Listener
             return new ArrayList<>();
         }
 
+        if (args.length > 3)
+            return new ArrayList<>();
+
         return super.onTabComplete(sender, command, alias, args);
     }
 
